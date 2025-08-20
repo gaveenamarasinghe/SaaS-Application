ea4537789ab44ed3982fc61ebdc07eb1cba1271c
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-08-20 14:24:07 +0530

    update page.tsx code

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,50 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/ui/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to My SaaS App</h1>
-      <button>
-        Let's get Started
-      </button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+      
+     <section className='home-section'>
+      <CompanionCard
+           id="123"
+           name="Neura the Brainy Explorer"
+           topic="Neural Network of the Brain"
+           subject="science"
+           duration="{45}"
+           color="#ffda6e"
+      />
+
+      <CompanionCard
+           id="456"
+           name="Neura the Brainy Explorer"
+           topic="Neural Network of the Brain"
+           subject="science"
+           duration="{45}"
+           color="#ffda6e"
+      />
+
+      <CompanionCard
+           id="789"
+           name="Neura the Brainy Explorer"
+           topic="Neural Network of the Brain"
+           subject="science"
+           duration="{45}"
+           color="#ffda6e"
+      />
+
+     </section>
+
+     <section className='home-section'>
+      <CompanionList/>
+      <CTA/>
+     </section>
+
+    </main>
   )
 }
 
