f8c0a06e1fa115973e5ae4242588907566134433
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-09-01 10:29:04 +0530

    Add tsx files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-import Navbar from "@/components/ui/Navbar";
+import Navbar from "@/components/Navbar";
+
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -21,9 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <Navbar/>
-        {children}
-        </body>
+        <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }>
+          <Navbar />
+          {children}
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,37 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
-import CompanionCard from '@/components/ui/CompanionCard'
-import CompanionList from '@/components/CompanionList'
-import CTA from '@/components/CTA'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+    const companions = await getAllCompanions({ limit: 3 });
+    const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
     <main>
-      <h1 className='text-2xl underline'>Popular Companions</h1>
-      
-     <section className='home-section'>
-      <CompanionCard
-           id="123"
-           name="Neura the Brainy Explorer"
-           topic="Neural Network of the Brain"
-           subject="science"
-           duration="{45}"
-           color="#ffda6e"
-      />
+      <h1>Popular Companions</h1>
 
-      <CompanionCard
-           id="456"
-           name="Neura the Brainy Explorer"
-           topic="Neural Network of the Brain"
-           subject="science"
-           duration="{45}"
-           color="#ffda6e"
-      />
+        <section className="home-section">
+            {companions.map((companion) => (
+                <CompanionCard
+                    key={companion.id}
+                    {...companion}
+                    color={getSubjectColor(companion.subject)}
+                />
+            ))}
 
-      <CompanionCard
-           id="789"
-           name="Neura the Brainy Explorer"
-           topic="Neural Network of the Brain"
-           subject="science"
-           duration="{45}"
-           color="#ffda6e"
-      />
+        </section>
 
-     </section>
-
-     <section className='home-section'>
-      <CompanionList/>
-      <CTA/>
-     </section>
-
+        <section className="home-section">
+            <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessionsCompanions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA />
+        </section>
     </main>
   )
 }
