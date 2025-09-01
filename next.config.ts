6e123ef902c8bb23cd4f4959a0077e76d67dd40f
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-09-01 10:11:20 +0530

    Add middleeware and sentry codes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['img.clerk.com'],
+  },
   /* config options here */
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "gaveen-amarasinghe",
+  project: "saas-app",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  disableLogger: true,
+  automaticVercelMonitors: true
+});
